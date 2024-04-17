6829bbe8909f405ccac13931970282daffa998a2
GABRIELpitchIN <gabriel@pitchin.my>
2024-04-17 22:09:28 +0800

    new img

--- backend/scripts/test.py
+++ backend/scripts/test.py
@@ -1,17 +1,17 @@
-import pickle
+import joblib
 import os
 
 # Get the directory of the current script
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 # Define the paths to your model files (using relative paths)
-model_pn_path = os.path.join(dir_path, 'models', 'model_pn.pkl')
-model_sev_path = os.path.join(dir_path, 'models', 'model_sev.pkl')
+model_pn_path = os.path.join(dir_path, 'models', 'model_pn.sav')
+model_sev_path = os.path.join(dir_path, 'models', 'model_sev.sav')
 
 # Load and inspect the model PN
 try:
     with open(model_pn_path, 'rb') as file:
-        model_pn = pickle.load(file)
+        model_pn = joblib.load(file)
     print("Model PN loaded successfully:")
     print(model_pn)
 except Exception as e:
@@ -21,7 +21,7 @@ except Exception as e:
 # Load and inspect the model SEV
 try:
     with open(model_sev_path, 'rb') as file:
-        model_sev = pickle.load(file)
+        model_sev = joblib.load(file)
     print("Model SEV loaded successfully:")
     print(model_sev)
 except Exception as e:
